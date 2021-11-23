483fe7afd2b165d2fcf5f146b7526c889088a9e6
murtada58 <murtada58@hotmail.com>
2021-11-23 16:20:38 +0000

    Refactored the pawn class

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -14,43 +14,41 @@ namespace Chessington.GameEngine.Pieces
             var availableMoves = new List<Square>();
             int numberOfStraightMoves = HasMoved ? 1 : 2;
             
+            this.AddAvailablePawnStraightMoves( board,
+                                                availableMoves,
+                                                currentSquare.Row,
+                                                currentSquare.Col,
+                                                (this.Player == Player.White ? -1 : 1),
+                                                numberOfStraightMoves);
+
+            
             var possibleCaptureMoves = new List<(int, int)>
             {
-                (currentSquare.Row + (this.Player == Player.Black ? 1 : -1), currentSquare.Col - 1),
-                (currentSquare.Row + (this.Player == Player.Black ? 1 : -1), currentSquare.Col + 1)
+                (currentSquare.Row + (this.Player == Player.White ? -1 : 1), currentSquare.Col - 1),
+                (currentSquare.Row + (this.Player == Player.White ? -1 : 1), currentSquare.Col + 1)
             };
-
-            switch (this.Player)
-            {
-                case GameEngine.Player.White:
-                    for (int i = 1; i <= numberOfStraightMoves; i++)
-                    {
-                        if (this.CanMoveTo(board, currentSquare.Row - i, currentSquare.Col))
-                        {
-                            var availableMove = Square.At(currentSquare.Row - i, currentSquare.Col);
-                            availableMoves.Add(availableMove);
-                        }
-                        else { break; }
-                    }
-                    
-                    this.AddMovesWhereCanCapture(board, availableMoves, possibleCaptureMoves);
-                    break;
-                
-                case GameEngine.Player.Black:
-                    for (int i = 1; i <= numberOfStraightMoves; i++)
-                    {
-                        if (this.CanMoveTo(board, currentSquare.Row + i, currentSquare.Col))
-                        {
-                            var availableMove = Square.At(currentSquare.Row + i, currentSquare.Col);
-                            availableMoves.Add(availableMove); 
-                        }
-                        else { break; }
-                    }
-                    this.AddMovesWhereCanCapture(board, availableMoves, possibleCaptureMoves);
-                    break;
-            }
+            this.AddMovesWhereCanCapture(board, availableMoves, possibleCaptureMoves);
 
             return availableMoves;
+        }
+        
+        public void AddAvailablePawnStraightMoves(  Board board,
+            List<Square> availableMoves,
+            int row,
+            int col,
+            int direction,
+            int numberOfStraightMoves
+        )
+        {
+            for (int i = direction; i * direction <= numberOfStraightMoves; i += direction)
+            {
+                if (this.CanMoveTo(board, row + i, col))
+                {
+                    var availableMove = Square.At(row + i, col);
+                    availableMoves.Add(availableMove); 
+                }
+                else { break; }
+            }
         }
     }
 }
