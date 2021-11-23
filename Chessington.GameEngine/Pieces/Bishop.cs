59cde25ec030b87507565fedde97594ac64b25cc
murtada58 <murtada58@hotmail.com>
2021-11-23 12:25:55 +0000

    Bishop can now move diagonally

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -10,7 +10,53 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            Square currentSquare = board.FindPiece(this);
+            var availableMoves = Enumerable.Empty<Square>();
+            // north east
+            int i = currentSquare.Row + 1;
+            int j = currentSquare.Col + 1;
+            while (i < GameSettings.BoardSize && j < GameSettings.BoardSize && board.GetPieceAtCoords(i, j) == null)
+            {
+                var availableMove = Square.At(i, j);
+                availableMoves = availableMoves.Append(availableMove);
+                i++;
+                j++;
+            }
+            
+            // north west
+            i = currentSquare.Row + 1;
+            j = currentSquare.Col - 1;
+            while (i < GameSettings.BoardSize && j >= 0 && board.GetPieceAtCoords(i, j) == null)
+            {
+                var availableMove = Square.At(i, j);
+                availableMoves = availableMoves.Append(availableMove);
+                i++;
+                j--;
+            }
+            
+            // south east
+            i = currentSquare.Row - 1;
+            j = currentSquare.Col + 1;
+            while (i >= 0 && j < GameSettings.BoardSize && board.GetPieceAtCoords(i, j) == null)
+            {
+                var availableMove = Square.At(i, j);
+                availableMoves = availableMoves.Append(availableMove);
+                i--;
+                j++;
+            }
+            
+            // south west
+            i = currentSquare.Row - 1;
+            j = currentSquare.Col - 1;
+            while (i >= 0 && j >= 0 && board.GetPieceAtCoords(i, j) == null)
+            {
+                var availableMove = Square.At(i, j);
+                availableMoves = availableMoves.Append(availableMove);
+                i--;
+                j--;
+            }
+            
+            return availableMoves;
         }
     }
 }
